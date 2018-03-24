4b6ce4803f8ff8ccee5f70fa437d7df462512c20
Abhishek Mishra <amishra42@sapient.com>
2018-03-24 17:04:24 +0000

    Issue fix nmp start

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-var express = require("express");
-var app = express();
+const path = require('path');
+const express = require("express");
+const app = express();
 
 const hostname = '127.0.0.1';
 const port = 3000;
-app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
-app.use('/build', express.static(path.join(__dirname, 'public/build')));
+app.use('/assets', express.static(path.join(__dirname, '/assets')));
+app.use('/build', express.static(path.join(__dirname, '/build')));
 
 
 
 
 app.get('/', function (req, res) {
-		res.sendfile(__dirname + '/index.html');
+		res.sendFile(__dirname + '/index.html');
 });
 app.listen(3000);
