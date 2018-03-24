4debfe846f410d95abceb8c082c2d75488d0b2a2
Abhishek Mishra <amishra42@sapient.com>
2018-03-24 17:12:32 +0000

    issue fix

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ const path = require('path');
 const express = require("express");
 const app = express();
 
-const hostname = '127.0.0.1';
-const port = 3000;
+app.set('port', (process.env.PORT || 5000))
+
 app.use('/assets', express.static(path.join(__dirname, '/assets')));
 app.use('/build', express.static(path.join(__dirname, '/build')));
-
-
-
 
 app.get('/', function (req, res) {
 		res.sendFile(__dirname + '/index.html');
 });
-app.listen(3000);
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
